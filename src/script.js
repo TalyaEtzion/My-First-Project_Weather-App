86520a92e9bad6f5e83d378d85dc935944463fcd
TalyaEtzion <67733722+TalyaEtzion@users.noreply.github.com>
2020-08-06 17:33:23 +0300

    Added JS for search bar input
    the id "city" will be updated according to the searched city

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 let now = new Date();
-console.log(now);
 let hour = now.getHours();
 if (hour < 10) {
   hours = `0${hour}`;
@@ -20,3 +19,19 @@ let days = [
 let day = days[now.getDay()];
 let date = document.querySelector("#date");
 date.innerHTML = `${day} ${hour}:${minutes}`;
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-bar");
+  let cityName = document.querySelector("#city");
+
+  if (searchInput.value) {
+    cityName.innerHTML = `${searchInput.value}`;
+  } else {
+    cityName.innerHTML = null;
+    alert("Please type a city");
+  }
+}
+
+let searchCity = document.querySelector(".form-group");
+searchCity.addEventListener("submit", search);
